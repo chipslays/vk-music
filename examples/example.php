c226aa856b8824e4942a64b77f64d372f91d5548
chipslays <hello@chipslays.ru>
2021-06-30 14:05:06 +0400

    cosmetic fix

--- examples/example.php
+++ examples/example.php
@@ -14,7 +14,7 @@ print_r($response->toArray());
 $response = $client->search('Justin Bieber - Baby');
 print_r($response->toArray());
 
-$response = $client->getById(["371745461_456289486", '-41489995_202246189']);
+$response = $client->getById(['1745461_456289486', '-41489995_202246189']);
 print_r($response->toArray());
 
 $response = $client->get("371745461");
